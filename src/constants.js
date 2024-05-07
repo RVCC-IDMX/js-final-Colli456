af758708f0cfd9ff83c01a0ee14e52b1539d4693
Colli456 <cjclarke14y@gmail.com>
2024-05-07 12:34:47 -0400

    Add Scripts

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,19 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I use it to work mostly on my JavaScript and Web Development project these days.
+    I also do some work in unity from time to time. 
+     Here is my <a href=" https://github.com/Colli456" target="_blank">Github</a>!`,
+   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. `, 
+   "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV. I use it for gaming and to watch a variety of different videos on YouTube, even tech videos from my favorite youtuber:
+  <a href=" https://www.youtube.com/@KevinPowell" target="_blank"> Kevin Powell</a>.`,
+  bed: `This is where I sleep. When I have free time, you will usually find me here if I’m not out and about with my friends.`,
+  resume: `This is my desk and on it is my resume. <a href="https://g00312583.wixsite.com/my-site-2/resume" target="_blank">Check it out?</a>
+  Contact me at cjclarke14y@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+  Kaboom.js by watching some of JSLegendDev  tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+  library: `Here is my bookshelf with my collection of various books. There are some textbooks and notebooks from my years in school (I don’t pick them up often).`,
+  exit: `If you want to exit from this portfolio, just close the tab.`,
+};
