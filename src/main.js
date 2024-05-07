6635adec19080adbe874638617169afff5784008
Colli456 <cjclarke14y@gmail.com>
2024-05-07 00:09:39 -0400

    Display Map

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
@@ -5,5 +6,23 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceY: 31,
     anims: {
         "idle-down": 936,
+        "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+        "idle-side": 975,
+        "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+        "idle-up": 1014,
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
     },
 });
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+k.screen("main", async () => {
+    const mapData = await (await fetch("./map.json")).json()
+    const layers = mapData.layers;
+
+    const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+});
+
+k.go("main");
