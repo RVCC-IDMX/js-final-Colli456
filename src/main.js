a079920988a978adc1f770bdd6d796aa389c780f
Colli456 <cjclarke14y@gmail.com>
2024-05-07 09:53:25 -0400

    Spawn / Movement

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
+import { displayDialogue } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -18,11 +19,11 @@ k.loadSprite("map", "./map.png");
 
 k.setBackground(k.Color.fromHex("#311047"));
 
-k.screen("main", async () => {
-    const mapData = await (await fetch("./map.json")).json()
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json();
     const layers = mapData.layers;
 
-    const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+    const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
 
     const player = k.make([
         k.sprite("spritesheet", {anim: "idle-down"}), k.area({
@@ -47,7 +48,7 @@ k.screen("main", async () => {
                     k.area({
                         shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
                     }),
-                    k.body({ isStatic: true}),
+                    k.body({ isStatic: true }),
                     k.pos(boundary.x, boundary.y),
                     boundary.name,
                 ]);
@@ -55,12 +56,37 @@ k.screen("main", async () => {
                 if (boundary.name) {
                     player.onCollide(boundary.name, () => {
                         player.isInDialogue = true;
-                        
+                        displayDialogue("TODO", () => (player.isInDialogue = false));
                     });
                 }
             }
+            continue;
+        }
+
+        if (layer.name === "spawnpoints") {
+            for (const entity of layer.objects) {
+              if (entity.name === "player") {
+                player.pos = k.vec2(
+                    (map.pos.x + entity.x) * scaleFactor,
+                    (map.pos.y + entity.y) * scaleFactor
+                );
+                k.add(player);
+                continue;
+              } 
+            }
         }
     }
+
+    k.onUpdate(() => {
+        k.camPos(player.pos.x, player.pos.y + 100);
+    });
+
+    k.onMouseDown((mouseBtn) => {
+        if (mouseBtn !== "left" || player.isInDialogue) return;
+
+        const worldMousePos = k.toWorld(k.mousePos());
+        player.moveTo(worldMousePos, player.speed);
+    });
 });
 
 k.go("main");
